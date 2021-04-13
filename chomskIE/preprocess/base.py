6fc9f88613d664b19a650f02cb3d30c64889ef00
aashishyadavally <aashish.yadavally1995@gmail.com>
2021-04-12 17:28:14 -0700

    Add __call__ method to base preprocessing class

--- chomskIE/preprocess/base.py
+++ chomskIE/preprocess/base.py
@@ -1,37 +1,33 @@
-"""This script defines an abstract base :class: `Preprocesser`
-with abstract methods :func: `fit` and :func: `transform,
-which transform the input data based on each preprocessing
-subclass' functionality.
-"""
-
-
 from abc import ABC, abstractmethod
 
 
 class Preprocesser(ABC):
-	"""An abstract class with :func: `fit` and :func: `transform`
-	which preprocesses the given data based on it's functionality.
-	"""
-	@abstractmethod
-	def _validate_input(self):
-		"""An abstract method which needs to be declared in the
-		subclasses. Checks whether the format of input matches
-		that expected by the subclass' :fit: method.
-		"""
-		pass
+    """An abstract base class for all preprocessing techniques.
+    """
+    @property
+    @abstractmethod
+    def name(self):
+        """Key for preprocessing technique. Helps identify preprocessing
+        technique output in `chomskIE.utils.Document.sents`.
+        """
+        pass
 
-	@abstractmethod
-	def fit(self):
-		"""An abstract method which needs to be declared in the
-		subclasses. Transforms training data according to the
-		functionality of the subclass.
-		"""
-		pass
+    @abstractmethod
+    def __call__(self):
+        """Apply the preprocessing technique of subclass to a stream
+        of documents.
+        """
+        pass
 
-	@abstractmethod
-	def transform(self):
-		"""An abstract method which needs to be declared in the
-		subclasses. Transforms given sentence according to the
-		functionality of the subclass.
-		"""
-		pass
+    @abstractmethod
+    def _validate_input(self):
+        """Check whether the format of input matches that expected
+        by the subclass' `__call__` and `transform` methods.
+        """
+        pass
+
+    @abstractmethod
+    def transform(self):
+        """Apply the preprocessing technique of subclass to single document.
+        """
+        pass
