68d9bc7d30b4799ef81a91950cc94093b236addd
aashishyadavally <aashish.yadavally1995@gmail.com>
2021-04-12 17:30:16 -0700

    Add skeleton workflow for pipeline

---
+++ chomskIE/__main__.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from chomskIE.dataset import Loader
+from chomskIE.utils import retrieve_spacy_language
+from chomskIE.preprocess import *
+
+
+LANGUAGE = 'en_core_web_sm'
+
+DATA_PATH = Path('/home/aashish/Desktop/raw')
+
+TRANSFORM = False
+
+PIPELINE = [
+    SentenceRecognizer,
+    WordTokenizer,
+]
+
+
+if __name__ == '__main__':
+    english_model = retrieve_spacy_language(lang=LANGUAGE)
+    data_loader = Loader()
+    if not TRANSFORM:
+        docs = data_loader.load_from_path(DATA_PATH)
+        for Pipe in PIPELINE:
+            pipe = Pipe(english_model)
+            docs = pipe(docs)
+
+        for doc in docs:
+            doc.processed = True
+
+    else:
+        doc = data_loader.load(DATA_PATH)
+        for Pipe in self.args:
+            pipe = Pipe(english_model)
+            doc = pipe.transform(doc)
+        doc.processed = True
